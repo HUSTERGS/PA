48536cfd1a63ca292eecfa83f7e391e59789f24b
tracer-ics2019 <tracer@njuics.org>
2021-01-07 13:11:30 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:11:30 up 1 day, 5 min,  2 users,  load average: 0.01, 0.01, 0.00 23858f50d32e387c42f2f209e30c8ca4f47e3f2f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -72,23 +72,23 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  // size_t i = 0;
-  // while (s1[i] != '\0' && s1[i] == s2[i]) {
-  //   i++;
-  //   if (i == n) {
-  //     return 0; 
-  //   }
-  // }
-  // return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
-  int D_value = 0;
-  size_t i    = 0;
-  
-  for (i = 0; *s1 && *s2 && i < n && D_value != 0; i++) {
-    s1++; s2++;
-  	D_value = *s1 - *s2;
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] == s2[i]) {
+    i++;
+    if (i == n) {
+      return 0; 
+    }
   }
+  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
+  // int D_value = 0;
+  // size_t i    = 0;
+  
+  // for (i = 0; *s1 && *s2 && i < n && D_value != 0; i++) {
+  //   s1++; s2++;
+  // 	D_value = *s1 - *s2;
+  // }
 
-  return D_value;
+  // return D_value;
 }
 
 void* memset(void* v,int c,size_t n) {
