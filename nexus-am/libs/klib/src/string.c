3d6481f75ed1e07bce988ab7649212035ee96bb8
tracer-ics2019 <tracer@njuics.org>
2021-01-07 13:13:23 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:13:23 up 1 day, 7 min,  2 users,  load average: 0.05, 0.03, 0.01 3f7208f64732e5c57420c492e33ee8e776d7385d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -59,17 +59,17 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  // unsigned char * ptr = v;
-  // unsigned char val = c;
-  // while (--n >= 0) {
-  //   ptr[n] = val;
-  // }
-  // return v;
-  const unsigned char uc = c; /* only cat the lowest c */
-  unsigned char *vu;
-  for (vu = v; 0 < n; ++vu, --n)
-	  *vu = uc;
+  unsigned char * ptr = v;
+  unsigned char val = c;
+  while (n-- >= 0) {
+    ptr[n] = val;
+  }
   return v;
+  // const unsigned char uc = c; /* only cat the lowest c */
+  // unsigned char *vu;
+  // for (vu = v; 0 < n; ++vu, --n)
+	//   *vu = uc;
+  // return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
