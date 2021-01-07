b8ff463bfcfc46139fadb30143894c0c0d080257
tracer-ics2019 <tracer@njuics.org>
2021-01-07 12:48:44 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:48:44 up 23:42,  2 users,  load average: 0.10, 0.03, 0.01 aea450c4fc23e414c6057f05fe5781ecb9e8735

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,116 +1,79 @@
 #include "klib.h"
- 
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-/* dhrystone 原来一直测试过不去，一直死循环，通过一些替换，发现是strcpy的锅
- * 参考了https://github.com/CX-514/ics2019.git，
- *
- */
+
 size_t strlen(const char *s) {
-  size_t i = 0;
-  while(s[i] && s[i] != '\0') i++;
-  return i;
+  size_t len = 0;
+  while(*s++ != '\0') {
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  /* 错误的实现方法
-   * return strncpy(dst, src, strlen(dst) );
-   *
-   * 正确的实现方法
-   * return strncpy(dst, src, strlen(src));但这种方法效率比较低下
-   */
-
-  size_t i;
-  for(i=0;src[i]!='\0';i++) 
-    dst[i]=src[i];
-  
-  dst[i]='\0';
+  strncpy(dst, src, strlen(src));
   return dst;
 }
 
-char *strncpy(char* dst, const char* src, size_t n) {
-  size_t i;
-  for (i = 0; i < n && src[i]!= '\0'; i++) dst[i] = src[i];
-  for (; i < n; i++) dst[i] = '\0';
+char* strncpy(char* dst, const char* src, size_t n) {
+  size_t index = 0;
+  while(src[index] != '\0' && index++ < n) {
+    dst[index] = src[index];
+  }
+  // 如果src的长度小于n的话，那么就在dst的后面填充'\0'
+  while (index < n) {
+    dst[index++] = '\0';
+  }
   return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  
-  /* 虽然这种方法也可以通过，但是性能非常非常的低下
-   * strcpy (dst + strlen(src), src);
-   * return dst;
-   */
-  
-  char *str=dst;
-  assert(dst!=NULL && src!=NULL);
-  while(*dst!='\0') {
-    dst++;
-  }
-  while((*dst++ = *src++) != '\0');
-  return str;
+  strcpy(dst + strlen(dst), src);
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  // size_t s1_size = strlen(s1);
-  // size_t s2_size = strlen(s2);
-  // size_t n = s1_size < s2_size ? s1_size : s2_size;
-  // return strncmp(s1, s2, n);
-
-  uint32_t i = 0;
-  for (i = 0; s1[i] == s2[i]; i++) if(s1[i] == '\0') return 0;
-
-  return ((signed char *)s1)[i] - ((signed char *)s2)[i];
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] == s2[i]) {
+    i++;
+  }
+  return (unsigned char) s1[i] - (unsigned char) s2[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  
-  /* 虽然可以通过测试，但是性能太低了
-  // size_t s1_size = strlen(s1);
-  // size_t s2_size = strlen(s2);
-  
-  // if (s1_size < n) n = s1_size;
-  // if (s2_size < n) n = s2_size;
-  
-  // uint32_t i = 0;
-  // for (i = 0; i < n; i++) {
-  // 	int D_value = s1[i] - s2[i];
-  // 	if (D_value) return D_value;
-  // }
-  // return 0;
-  */
-  int D_value = 0;
-  size_t i    = 0;
-  
-  for (i = 0; *s1 && *s2 && i < n && D_value != 0; i++) {
-    s1++; s2++;
-  	D_value = *s1 - *s2;
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] == s2[i]) {
+    i++;
+    if (i == n) {
+      return 0; 
+    }
   }
-
-  return D_value;
+  return (unsigned char) s1[i] - (unsigned char) s2[i];
 }
 
 void* memset(void* v,int c,size_t n) {
-  const unsigned char uc = c; /* only cat the lowest c */
-  unsigned char *vu;
-  for (vu = v; 0 < n; ++vu, --n)
-	  *vu = uc;
+  unsigned char * ptr = (unsigned char *)v;
+  unsigned char val = (unsigned char)c;
+  while (--n >= 0) {
+    ptr[n] = val;
+  }
   return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  const char *src = in;
-  char *dst = out;
-
-  while (n--) *dst++ = *src++;
-
+  const unsigned char *src = in;
+  unsigned char * dst = out;
+  while (n--) {
+    *dst++ = *src++;
+  }
   return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
   if(!n) return 0;
   while(--n && *(char *)s1 == *(char *)s2) {
-	s1 = (char *)s1 + 1;
-	s2 = (char *)s2 + 1;
+	  s1 = (char *)s1 + 1;
+	  s2 = (char *)s2 + 1;
   }
   return (*((unsigned char *)s1) - *((unsigned char *)s2));
 }
