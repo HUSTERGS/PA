60af3ab8e021b28ce9b4f64c6a869dbd4cc12934
tracer-ics2019 <tracer@njuics.org>
2021-01-07 13:11:16 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:11:16 up 1 day, 5 min,  2 users,  load average: 0.01, 0.01, 0.00 7454397a90214e154999cdbcb5fd8bdee24f7d14

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,24 +1,28 @@
 #include "klib.h"
- 
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-/* dhrystone 原来一直测试过不去，一直死循环，通过一些替换，发现是strcpy的锅
- * 参考了https://github.com/CX-514/ics2019.git，
- *
- */
+
 size_t strlen(const char *s) {
+  // size_t len = 0;
+  // while(s[len] != '\0') {
+  //   len++;
+  // }
+  // return len;
   size_t i = 0;
   while(s[i] && s[i] != '\0') i++;
   return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  /* 错误的实现方法
-   * return strncpy(dst, src, strlen(dst) );
-   *
-   * 正确的实现方法
-   * return strncpy(dst, src, strlen(src));但这种方法效率比较低下
-   */
-
+  
+  // size_t i = 0;
+  // while (src[i] != '\0') {
+  //   dst[i] = src[i];
+  //   i++;
+  // }
+  // assert(src[i] == '\0');
+  // dst[i] = '\0';
+  // return dst;
   size_t i;
   for(i=0;src[i]!='\0';i++) 
     dst[i]=src[i];
@@ -27,7 +31,16 @@ char *strcpy(char* dst,const char* src) {
   return dst;
 }
 
-char *strncpy(char* dst, const char* src, size_t n) {
+char* strncpy(char* dst, const char* src, size_t n) {
+  // size_t index = 0;
+  // while(src[index] != '\0' && index++ < n) {
+  //   dst[index] = src[index];
+  // }
+  // // 如果src的长度小于n的话，那么就在dst的后面填充'\0'
+  // while (index < n) {
+  //   dst[index++] = '\0';
+  // }
+  // return dst;
   size_t i;
   for (i = 0; i < n && src[i]!= '\0'; i++) dst[i] = src[i];
   for (; i < n; i++) dst[i] = '\0';
@@ -35,12 +48,8 @@ char *strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  
-  /* 虽然这种方法也可以通过，但是性能非常非常的低下
-   * strcpy (dst + strlen(src), src);
-   * return dst;
-   */
-  
+  // strcpy(dst + strlen(dst), src);
+  // return dst;
   char *str=dst;
   assert(dst!=NULL && src!=NULL);
   while(*dst!='\0') {
@@ -51,11 +60,11 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  // size_t s1_size = strlen(s1);
-  // size_t s2_size = strlen(s2);
-  // size_t n = s1_size < s2_size ? s1_size : s2_size;
-  // return strncmp(s1, s2, n);
-
+  // size_t i = 0;
+  // while (s1[i] != '\0' && s1[i] == s2[i]) {
+  //   i++;
+  // }
+  // return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
   uint32_t i = 0;
   for (i = 0; s1[i] == s2[i]; i++) if(s1[i] == '\0') return 0;
 
@@ -63,21 +72,14 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  
-  /* 虽然可以通过测试，但是性能太低了
-  // size_t s1_size = strlen(s1);
-  // size_t s2_size = strlen(s2);
-  
-  // if (s1_size < n) n = s1_size;
-  // if (s2_size < n) n = s2_size;
-  
-  // uint32_t i = 0;
-  // for (i = 0; i < n; i++) {
-  // 	int D_value = s1[i] - s2[i];
-  // 	if (D_value) return D_value;
+  // size_t i = 0;
+  // while (s1[i] != '\0' && s1[i] == s2[i]) {
+  //   i++;
+  //   if (i == n) {
+  //     return 0; 
+  //   }
   // }
-  // return 0;
-  */
+  // return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
   int D_value = 0;
   size_t i    = 0;
   
@@ -90,6 +92,12 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
+  // unsigned char * ptr = v;
+  // unsigned char val = c;
+  // while (--n >= 0) {
+  //   ptr[n] = val;
+  // }
+  // return v;
   const unsigned char uc = c; /* only cat the lowest c */
   unsigned char *vu;
   for (vu = v; 0 < n; ++vu, --n)
@@ -98,19 +106,19 @@ void* memset(void* v,int c,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  const char *src = in;
-  char *dst = out;
-
-  while (n--) *dst++ = *src++;
-
+  const unsigned char *src = in;
+  unsigned char * dst = out;
+  while (n--) {
+    *dst++ = *src++;
+  }
   return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
   if(!n) return 0;
   while(--n && *(char *)s1 == *(char *)s2) {
-	s1 = (char *)s1 + 1;
-	s2 = (char *)s2 + 1;
+	  s1 = (char *)s1 + 1;
+	  s2 = (char *)s2 + 1;
   }
   return (*((unsigned char *)s1) - *((unsigned char *)s2));
 }
