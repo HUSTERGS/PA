529a9c5ad0cc76d62fbf665396819c59f1d66304
tracer-ics2019 <tracer@njuics.org>
2021-01-07 17:35:05 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:35:04 up 1 day,  4:28,  2 users,  load average: 0.08, 0.02, 0.01 f024be9e3a61acc028ad6af3feb92874b27c897

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -2,45 +2,98 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-// 将参数列表的参数，int/str转化为param，然后进行打印
-static struct param {
-  char * buffer; // 用于将参数转化为字符之后保存数据
-  int value;
-  const char * str;
-  short sign; // 正负数
-};
+// // 将参数列表的参数，int/str转化为param，然后进行打印
+// static struct param {
+//   char * buffer; // 用于将参数转化为字符之后保存数据
+//   int value;
+//   const char * str;
+//   short sign; // 正负数
+// };
+// 返回打印的字符的个数
+
+// 根据dst是否为NULL来判断应该输出到标准输出还是输出到dst
+static void putc(char * dst, char ch, int offset) {
+  if (dst) {
+    dst[offset] = ch;
+  } else {
+    _putc(ch);
+  }
+}
+
+
+static int print_s(const char * data, char * dst) {
+  const char * count = data;
+  while (*count) {
+    putc(dst, *count, count - data);
+    count++;
+  }
+  return count - data;
+}
+
+static int print_d(int d, int count, char * dst) {
+  if (d < 0) {
+    putc(dst, '-', count);
+    return print_d(-d, count, dst+1) +1;
+  }
+  if (d / 10) {
+    count += print_d(d / 10, count, dst);
+    putc(dst, d % 10 + '0', count);
+  } else {
+    putc(dst, d + '0', count);
+  }
+  return count + 1;
+}
+
+
 
 int printf(const char *fmt, ...) {
-  
-  while (*fmt != '\0') {
-    // 遇到转义
-    if (*fmt == '%') {
-      switch (*(fmt+1))
-      {
-      case 'd':
-        // 如果是数字
-        break;
-      case 's':
-        // 如果是字符串
-        break;
-      case '%':
-        _putc('%');
-      default:
-        assert(0);
-      }
-    }
-  }
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int count = vsprintf(NULL, fmt, ap);
+  putc(NULL, '\0', count);
+  va_end(ap);
+  return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  char *s;
+  int d;
+  char ch;
+  int count = 0;
+  while(ch = *fmt++) {
+    if (ch != '%') {
+      _putc(ch);
+      count++;
+      continue;
+    }
+    switch (*fmt++)
+    {
+    case 's':
+      s = va_arg(ap, char *);
+      count += out ? print_s(s, out + count) : print_s(s, NULL);
+      break;
+    case 'd':
+      d = va_arg(ap, int);
+      count += out ? print_d(d, 0, out + count) : print_d(d, 0, NULL);
+      break;
+    default:
+      break;
+    }
+  }
+  return count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int count = vsprintf(out, fmt, ap);
+  va_end(ap);
+  // 应该最后要打印一个结束符号？
+  putc(out, '\0', count);
+  return count;
 }
 
+// TODO: snprintf
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
 }
