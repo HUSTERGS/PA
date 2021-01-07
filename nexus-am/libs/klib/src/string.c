f68bb3d1766a26230a2331054f703ee0e1afbe9d
tracer-ics2019 <tracer@njuics.org>
2021-01-07 13:10:04 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:10:04 up 1 day, 3 min,  2 users,  load average: 0.05, 0.01, 0.00 4959e69d1c2352ed55e08098e243ea3f11c40dd2

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,11 +3,14 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t len = 0;
-  while(s[len] != '\0') {
-    len++;
-  }
-  return len;
+  // size_t len = 0;
+  // while(s[len] != '\0') {
+  //   len++;
+  // }
+  // return len;
+  size_t i = 0;
+  while(s[i] && s[i] != '\0') i++;
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
