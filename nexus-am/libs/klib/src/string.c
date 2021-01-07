55137412826b4bd00659905a798eb4bc1ac362fd
tracer-ics2019 <tracer@njuics.org>
2021-01-07 13:10:23 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:10:23 up 1 day, 4 min,  2 users,  load average: 0.03, 0.01, 0.00 948acbae596f03aee6784b0bbbc9f5b5a73c683b

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -60,11 +60,15 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  size_t i = 0;
-  while (s1[i] != '\0' && s1[i] == s2[i]) {
-    i++;
-  }
-  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
+  // size_t i = 0;
+  // while (s1[i] != '\0' && s1[i] == s2[i]) {
+  //   i++;
+  // }
+  // return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
+  uint32_t i = 0;
+  for (i = 0; s1[i] == s2[i]; i++) if(s1[i] == '\0') return 0;
+
+  return ((signed char *)s1)[i] - ((signed char *)s2)[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
