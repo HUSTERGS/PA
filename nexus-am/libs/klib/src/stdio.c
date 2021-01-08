775a0982e1b0a58afd0d3230e556706287de6a47
tracer-ics2019 <tracer@njuics.org>
2021-01-08 11:54:11 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:54:11 up 1 day, 22:48,  2 users,  load average: 0.10, 0.06, 0.02 3d985811eb2fab50b9418f3ba27cdebc4a7d6dab

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -90,7 +90,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   // assert(0);
-  printf("噜啦啦");
+  // printf("噜啦啦");
   va_list ap;
   va_start(ap, fmt);
   int count = vsprintf(out, fmt, ap);
