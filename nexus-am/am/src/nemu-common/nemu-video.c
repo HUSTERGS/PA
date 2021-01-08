ad8cd7525c8746dc19161e5cb5de5ac554d0aefc
tracer-ics2019 <tracer@njuics.org>
2021-01-08 18:38:12 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:38:11 up  3:13,  1 user,  load average: 0.06, 0.02, 0.00 edde54b86e56969b2c4c304b6ab6c540d9a7d54f

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -55,6 +55,6 @@ void __am_vga_init() {
   int i;
   int size = screen_width() * screen_height();
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < size; i ++) fb[i] = i;
+  for (i = 0; i < size; i ++) fb[i] = 0;
   draw_sync();
 }
