61a14fffc2f4fc62b0873f98323d9da8b507e61f
tracer-ics2019 <tracer@njuics.org>
2021-01-08 17:04:27 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:04:27 up  1:39,  2 users,  load average: 0.23, 0.33, 0.17 f1a252a1a7f82e865abf82be1a9250a5c58b671a

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -33,7 +33,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       int W = screen_width();
       int H = screen_height();
       int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+      uint32_t *fb = (uint32_t *)FB_ADDR;
       for (int j = 0; j < h && y + j < H; j++){
         memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
         pixels += w;
