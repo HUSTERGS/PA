99085398babedb5b4c4fdb294c1131fe09ba2be3
tracer-ics2019 <tracer@njuics.org>
2021-01-08 16:57:47 +0800

    >  compile U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:57:47 up  1:32,  2 users,  load average: 0.13, 0.04, 0.01 25bc6700c1600da2a59de74a4afabd83141d6c64

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -30,7 +30,9 @@ static inline void update_screen() {
 
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
-  TODO();
+  // TODO();
+  assert(offset == 0);
+  update_screen();
 }
 
 void init_vga() {
