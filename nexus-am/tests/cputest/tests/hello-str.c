fc30735cdedc5d447d9824632b7d284dd2c1413a
tracer-ics2019 <tracer@njuics.org>
2021-01-08 12:05:16 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:05:16 up 1 day, 22:59,  2 users,  load average: 0.06, 0.13, 0.07 9d7eb9bff8bcef87142867f53acdc0d0692ebea7

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -3,16 +3,15 @@
 char buf[128];
 
 int main() {
-	sprintf(buf, "%s ", "Hello world!\n");
+
 	sprintf(buf, "%s", "Hello world!\n");
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	// nemu_assert(strcmp(buf, "Hello world!\n") == 0);
-	// sprintf(buf, "%d ", 2);
-	// sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	// nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
-	// sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	// nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
+	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
 }
