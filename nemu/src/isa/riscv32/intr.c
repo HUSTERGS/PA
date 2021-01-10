afc9d353da3a1240b42bc0aa88e6d632c99d9d3f
tracer-ics2019 <tracer@njuics.org>
2021-01-10 21:43:19 +0800

    >  compile U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:43:19 up  5:58,  2 users,  load average: 0.24, 0.17, 0.06 633afe1d1141f76f982f69fd48985633568119

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -20,7 +20,8 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
   csr_write(SCAUSE, NO); /* 2.set the exception code to scause  */
   // t0 = csr_read(STVEC);  /* 3.get the exception entry address from stvec register */ 
   interpret_rtl_j(csr_read(STVEC)); /* 4.jump to exception entry address   */
-  Log("到了这个地方");
+  // Log("reach this place");
+  assert(0);
 }
 
 bool isa_query_intr(void) {
