5efcd0d36aef3ad878eb26575079e3bcf4da70d6
tracer-ics2019 <tracer@njuics.org>
2021-01-11 17:09:04 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:09:04 up 1 day,  1:23,  2 users,  load average: 0.17, 0.41, 0.42 a393da425670e3ff7e0f1f062c1e67633ce46430

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -7,7 +7,7 @@ int main() {
   // write(1, "Hello World!\n", 13);
   // write(1, "Hello World!\n", 13);
   // write(1, "Hello World!\n", 13);
-  // printf("111");
+  printf("");
   // int i = 2;
   // volatile int j = 0;
   // // while (1) {
