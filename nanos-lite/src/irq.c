cdc1d8a312f891161f085ae3951525f922360ec6
tracer-ics2019 <tracer@njuics.org>
2021-01-11 13:16:29 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:16:29 up 21:31,  2 users,  load average: 0.12, 0.20, 0.18 2f616c797a573e3685cdead5427f06faab7cae2f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include "common.h"
 _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
+  Log("进入了do_event函数，event类型为%d", e.event);
   switch (e.event) {
     case _EVENT_YIELD   : printf("self int\n");  break;
     case _EVENT_SYSCALL : do_syscall(c);         break;
@@ -12,6 +13,6 @@ static _Context* do_event(_Event e, _Context* c) {
 
 void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
-  // 注册了事件处理回调函数
+  // 注册了
   _cte_init(do_event);
 }
