adf1c7ba968a7730dfe2cb4f01947e3886080fa0
tracer-ics2019 <tracer@njuics.org>
2021-01-11 11:50:42 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:50:42 up 20:05,  2 users,  load average: 0.01, 0.36, 0.40 71f03ec1797b470279517e755309c3cb66e49e06

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,8 +1,9 @@
 #include <stdint.h>
-
+#include <stdio.h>
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+  // Log("进入dummy");
   return _syscall_(SYS_yield, 0, 0, 0);
 }
