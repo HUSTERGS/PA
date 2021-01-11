c7b4027b479c1165d5b75d56af796236f1cfb7c8
tracer-ics2019 <tracer@njuics.org>
2021-01-11 12:20:52 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:20:52 up 20:35,  2 users,  load average: 0.72, 0.44, 0.47 ebf2b4e2d6252ec7d182ee75592e4880ef78cbe2

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
   // Log("进入dummy");
-  printf("进入dummy");
+  // printf("进入dummy");
+  assert(0);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
