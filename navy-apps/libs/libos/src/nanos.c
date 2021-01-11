ec3c54df753112f8e2134550d247f6eb3cfaea3d
tracer-ics2019 <tracer@njuics.org>
2021-01-11 15:32:09 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:32:09 up 23:46,  2 users,  load average: 0.00, 0.03, 0.01 553387642ecccd4875da38bde154cfe090e501be

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -67,7 +67,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  _write(1, "printf function2\n", 17);
+  // _write(1, "printf function2\n", 17);
   return (void *)-1;
 }
 
