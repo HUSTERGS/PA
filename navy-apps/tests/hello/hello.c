82578973837363f1bee87765c17418aaaf01ac1b
tracer-ics2019 <tracer@njuics.org>
2021-01-11 16:24:02 +0800

    >  run U201714668 hust Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:24:02 up 1 day, 38 min,  2 users,  load average: 0.05, 0.10, 0.04 16722c2fd455cf3d59287d2079557555e683f96

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -8,16 +8,16 @@ int main() {
   write(1, "Hello World!\n", 13);
   write(1, "Hello World!\n", 13);
   printf("111");
-  int i = 2;
-  volatile int j = 0;
-  // while (1) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
-  // printf("111");
-  write(1, "Exit hello.c file\n", 18);
+  // int i = 2;
+  // volatile int j = 0;
+  // // while (1) {
+  // //   j ++;
+  // //   if (j == 10000) {
+  // //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  // //     j = 0;
+  // //   }
+  // // }
+  // // printf("111");
+  // write(1, "Exit hello.c file\n", 18);
   return 0;
 }
